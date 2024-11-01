16368285d2ff7a82d978f78f72defed9e9bda2a1
korirsoftwaredeveloper <70626631+Vicko-maker@users.noreply.github.com>
2024-11-01 14:30:57 +0300

    Update urls.py

--- loginapp/urls.py
+++ loginapp/urls.py
@@ -7,4 +7,5 @@ from . import views
 
 urlpatterns = [
     path('', views.Register.as_view(), name='login'),
-]+static(settings.MEDIA_URL,  document_root=settings.MEDIA_ROOT)
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
+  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
