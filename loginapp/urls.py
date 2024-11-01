eb12df0a679f36326b540d76fb806544edc64cca
korirsoftwaredeveloper <70626631+Vicko-maker@users.noreply.github.com>
2024-11-01 14:23:53 +0300

    Update urls.py

--- loginapp/urls.py
+++ loginapp/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.auth import views as auth_view
 from . import views
 
 urlpatterns = [
